ff01e74f29537bd45e68c6e523109a563329e4a9
sushmith <6890568+bsushmith@users.noreply.github.com>
2024-05-29 15:06:00 +0530

    feat: init bloom filter based on expected false probability

--- bloom_filter/bloom_filter.go
+++ bloom_filter/bloom_filter.go
@@ -4,47 +4,76 @@ package bloom_filter
 import (
 	"github.com/cespare/xxhash"
 	"github.com/spaolacci/murmur3"
-	"hash"
+	"math"
 	"time"
 )
 
 type BloomFilter struct {
-	bits    *BitArray
-	size    uint64
-	hashFn1 hash.Hash64
-	hashFn2 hash.Hash64
+	bits      *BitArray
+	size      uint64
+	hashFuncs []func(string, uint64) uint64
 }
 
-func NewBloomFilter(size uint64) *BloomFilter {
-	return &BloomFilter{
-		bits:    NewBitArray(size),
-		size:    size,
-		hashFn1: murmur3.New64WithSeed(uint32(time.Now().Unix())),
-		hashFn2: xxhash.New(),
+// NewBloomFilter creates a new bloom filter with the given capacity and false positive probability
+// The capacity is the expected number of elements to be added to the filter
+// The false positive probability is the probability of the filter returning true for an element that was not added
+// Ref: https://en.wikipedia.org/wiki/Bloom_filter#Optimal_number_of_hash_functions
+func NewBloomFilter(capacity uint64, falsePositiveProbability float64) *BloomFilter {
+	bitArraySize := -1 * (float64(capacity) * math.Log(falsePositiveProbability)) / (math.Pow(math.Log(2), 2))
+	numHashFunctions := int((bitArraySize / float64(capacity)) * math.Log(2))
+	bloomFilter := BloomFilter{
+		bits:      NewBitArray(uint64(bitArraySize)),
+		size:      uint64(bitArraySize),
+		hashFuncs: getHashFunctions(numHashFunctions, uint64(bitArraySize)),
 	}
+	return &bloomFilter
 }
 
-func (b *BloomFilter) applyHash(data string, size uint64) (uint64, uint64) {
-	b.hashFn1.Write([]byte(data))
-	h1 := b.hashFn1.Sum64() % size
-	b.hashFn1.Reset()
+func getHashFunctions(numHashFunctions int, size uint64) []func(string, uint64) uint64 {
+	hashFn1 := murmur3.New64WithSeed(uint32(time.Now().Unix()))
+	hashFn2 := xxhash.New()
+	hashFunctions := make([]func(string, uint64) uint64, numHashFunctions)
+	//hash functions of the form ah1 + bh2
+	for i := 0; i < numHashFunctions; i++ {
+		hashFunctions[i] = func(data string, size uint64) uint64 {
+			hashFn1.Write([]byte(data))
+			hashFn2.Write([]byte(data))
+			hash1 := (i * int(hashFn1.Sum64())) % int(size)
+			hash2 := (i * i * int(hashFn2.Sum64())) % int(size)
+			hashFn1.Reset()
+			hashFn2.Reset()
+			return uint64(hash1+hash2) % size
+		}
+	}
 
-	b.hashFn2.Write([]byte(data))
-	h2 := b.hashFn2.Sum64() % size
-	b.hashFn2.Reset()
+	return hashFunctions
+}
 
-	return h1, h2
+func (b *BloomFilter) applyHash(data string, size uint64) []uint64 {
+	hashes := make([]uint64, len(b.hashFuncs))
+	for i, hashFn := range b.hashFuncs {
+		hashes[i] = hashFn(data, size)
+	}
+	return hashes
 }
 
 func (b *BloomFilter) Add(element string) {
-	h1, h2 := b.applyHash(element, b.size)
-	b.bits.Set(h1)
-	b.bits.Set(h2)
+	hashes := b.applyHash(element, b.size)
+	for _, hash := range hashes {
+		b.bits.Set(hash)
+	}
 }
 
 func (b *BloomFilter) Exists(element string) bool {
-	h1, h2 := b.applyHash(element, b.size)
-	return b.bits.IsSet(h1) && b.bits.IsSet(h2)
+	hashes := b.applyHash(element, b.size)
+	exists := true
+	for _, hash := range hashes {
+		exists = exists && b.bits.IsSet(hash)
+		if !exists {
+			return false
+		}
+	}
+	return true
 }
 
 func (b *BloomFilter) ClearAll() {
